b714210f4e8ba3a31bd2d432e9a2eaff46783448
itsSunil001 <98368191+itsSunil001@users.noreply.github.com>
2024-01-17 00:09:48 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-import express, { text } from "express";
+import express from "express";
 import cors from "cors";
 import router from "./routes.js";
 import { con } from "./database.js";
 import {ErrorMiddleware} from "./error/error.js"
 const app = express();
-
+const port= process.env.PORT || 3001;
+app.use(cors({ origin: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 app.use("/images", express.static("upload/images"));
 con("ecom");
 app.use(router);
 app.use(ErrorMiddleware)
 
-app.listen(3000);
+app.listen(port);
